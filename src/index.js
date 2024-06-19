68d4ef118f5dce0dc7889bb61336d376c1f76611
system51 <system51@126.com>
2024-06-19 09:25:48 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.ikubernetes.xyz": dockerHub,
+  "quay.ikubernetes.xyz": "https://quay.io",
+  "gcr.ikubernetes.xyz": "https://gcr.io",
+  "k8s-gcr.ikubernetes.xyz": "https://k8s.gcr.io",
+  "k8s.ikubernetes.xyz": "https://registry.k8s.io",
+  "ghcr.ikubernetes.xyz": "https://ghcr.io",
+  "cloudsmith.ikubernetes.xyz": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.ikubernetes.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
